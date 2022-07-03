15cfcd28bd21b58d1124e9680660f36afd1a44b0
Tatsuya4610 <tatsuya7610@icloud.com>
2022-07-03 16:55:54 +0900

    adminサイトにTodoModel表示

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,9 @@
+import re
 from django.db import models
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=20)
     memo = models.TextField()
+
+    def __str__(self):
+        return self.title
