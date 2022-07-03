415b8845473ea8477f262cffe8beb54c5e2bb4d6
Tatsuya4610 <tatsuya7610@icloud.com>
2022-07-03 17:52:28 +0900

    detail.html作成

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from django.shortcuts import render
 
 from todo.models import TodoModel
 
 class TodoList(ListView):
     template_name = 'list.html'
+    model = TodoModel
+
+
+class TodoDetail(DetailView):
+    template_name = 'detail.html'
     model = TodoModel
