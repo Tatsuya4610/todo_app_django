f8a55a3397e75d26c4963dbcf1d0f40afa7f2724
Tatsuya4610 <tatsuya7610@icloud.com>
2022-07-05 00:21:59 +0900

    todo追加ページの実装

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView,DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView
 from django.shortcuts import render
 
 from todo.models import TodoModel
@@ -11,3 +12,10 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title','memo','priority','date')
+    # reverse_lazyはtodo/urls.py内のname='list'に紐付け、POST成功時にそのViewへ遷移させる。
+    success_url = reverse_lazy('list')
